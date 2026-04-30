bf6ffd7379de7938f13eebcf5ed8a1c990281152
jw <jw@jwdeMacBook-Air.local>
2026-04-30 09:08:53 +0800

    Updates

--- _next/static/xnIBzneWHV3d_pA7hAVFh/_buildManifest.js
+++ _next/static/xnIBzneWHV3d_pA7hAVFh/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/thuyangbin//_next/:path+",
+        "destination": "/thuyangbin/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
